b50dadf86245362c84cd7101f925abd250a7161b
Garrett Snyder <garrettsnyder0@gmail.com>
2019-01-24 08:09:01 -0800

    Whiteboarding Exercises From MSSA

---
+++ Whiteboarding/FeetToInch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboarding
+{
+    class FeetToInch
+    {
+
+        static double ConvertFeetToInch(double feet) => feet * 12;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertFeetToInch(2.0));
+            Console.WriteLine(ConvertFeetToInch(2.5));
+
+            Console.ReadKey();
+        }
+    }
+}
