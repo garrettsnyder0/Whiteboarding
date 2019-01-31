fa71e822cf12634fe3e8909a67a32dc915e54fb3
Garrett Snyder <garrettsnyder0@gmail.com>
2019-01-31 12:03:41 -0800

    Reverse Characater Case problem

--- CountVowels/CountVowels.cs
+++ CountVowels/CountVowels.cs
@@ -13,13 +13,18 @@ namespace CountVowels
         {
             int vowelCount = 0;
             input = input.ToLower();
+            string check = "aeiou";
 
             for(int i = 0; i < input.Length; i++)
             {
-                if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                if (check.Contains(input[i]))
                 {
                     vowelCount++;
                 }
+                //if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                //{
+                //    vowelCount++;
+                //}
             }
 
             return vowelCount;
---
+++ ReverseCharacters/ReverseCharacters.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseCharacters
+{
+    class ReverseCharacters
+    {
+
+        //Given a string, return a string with upper and lower case letters fliped 
+        public static string ReverseChar(string input)
+        {
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string reversed = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                //if(Char.IsLower(input[i]))
+                if (lower.Contains(input[i]))
+                {
+                    reversed += Char.ToUpper(input[i]);
+                }
+                //else if(Char.IsUpper(input[i]))
+                else if (upper.Contains(input[i]))
+                {
+                    reversed += Char.ToLower(input[i]);
+                }
+                else
+                {
+                    reversed += input[i];
+                }
+            }
+
+
+            return reversed;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("LeTuce");
+            Console.WriteLine(ReverseChar("LeTuce"));
+            Console.WriteLine("MiSSIssIpPi");
+            Console.WriteLine(ReverseChar("MiSSIssIpPi"));
+            Console.WriteLine("La-A");
+            Console.WriteLine(ReverseChar("La-A"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
