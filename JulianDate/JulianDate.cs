f294de591cb7e7e86657c4aacdc59148d16885da
Garrett Snyder <garrettsnyder0@gmail.com>
2019-02-05 11:25:54 -0800

    Julian date and clock angle

---
+++ JulianDate/JulianDate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulianDate
+{
+    class JulianDate
+    {
+        //Given month and day calculate the julian date
+        public static int CalcJulianDate(int month, int day)
+        {
+            int julian = day;
+            for(int i = 1; i < month; i++)
+            {
+                if(i ==2)
+                {
+                    julian += 28;
+                }
+                else if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10)
+                {
+                    julian += 31;
+                }
+                else
+                {
+                    julian += 30;
+                }
+            }
+
+            return julian;
+        }
+
+        //Same as above using array
+        public static int CalcJulianDate2(int month, int day)
+        {
+            int julian = day;
+            int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 };
+
+            for (int i = 1; i < month; i++)
+            {
+                julian += daysInMonth[i-1];
+            }
+
+            return julian;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcJulianDate(1, 1));
+            Console.WriteLine(CalcJulianDate(2, 28));
+            Console.WriteLine(CalcJulianDate(12, 31));
+            Console.WriteLine(CalcJulianDate(7, 15));
+            Console.WriteLine(CalcJulianDate(10, 7));
+
+            Console.WriteLine(CalcJulianDate2(1, 1));
+            Console.WriteLine(CalcJulianDate2(2, 28));
+            Console.WriteLine(CalcJulianDate2(12, 31));
+            Console.WriteLine(CalcJulianDate2(7, 15));
+            Console.WriteLine(CalcJulianDate2(10, 7));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TimeAngle/TimeAngle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAngle
+{
+    class TimeAngle
+    {
+
+        //Given hour and minute calculate the angle between the hands on a clock
+        public static int AngleBetweenHands(int hour, int min)
+        {
+            int angle = 0;
+            int oneDiff = 30; //30 degrees between each number 360/12
+
+            if(min == 0)
+            {
+                min = 12;
+            }
+            else
+            {
+                min = min / 5;
+            }
+
+            if(hour > min)
+            {
+                angle = hour - min;
+            }
+            else
+            {
+                angle = min - hour;
+            }
+
+            angle *= oneDiff;
+
+            if(angle > 180)
+            {
+                angle -= 180;
+            }
+
+            return angle;
+        }
+
+        //Given hour and minute calculate the angle between the hands on a clock
+        //Way 2
+        //Now account for hour hand not being on hour
+        public static double AngleBetweenHands2(int hour, int min)
+        {
+            double angle = 0;
+            double oneDiff = 30;
+
+            double hourAngle = (double)hour * oneDiff;
+            hourAngle += ((30.0 / 60.0) * (min)); // travle 30 degrees in 60 min
+            double minAngle = 360;
+            if(min != 0)
+            {
+                minAngle = (double)(min / 5) * oneDiff;
+            }
+           
+
+            if (hourAngle > minAngle)
+            {
+                angle = hourAngle - minAngle;
+            }
+            else
+            {
+                angle = minAngle - hourAngle;
+            }
+
+            
+
+            if (angle > 180)
+            {
+                angle -= 180;
+            }
+
+            return angle;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(AngleBetweenHands(3,00));
+            Console.WriteLine(AngleBetweenHands(6, 45));
+            Console.WriteLine(AngleBetweenHands(2, 15));
+            Console.WriteLine(AngleBetweenHands(12, 00));
+            Console.WriteLine(AngleBetweenHands(10, 30));
+            
+            Console.WriteLine(AngleBetweenHands2(3, 00));
+            Console.WriteLine(AngleBetweenHands2(6, 45));
+            Console.WriteLine(AngleBetweenHands2(2, 15));
+            Console.WriteLine(AngleBetweenHands2(12, 00));
+            Console.WriteLine(AngleBetweenHands2(10, 30));
+
+            Console.ReadKey();
+        }
+    }
+}
