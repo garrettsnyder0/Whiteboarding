b7f9793c78ba2e62c6c8df34dbbc03ba076c7246
Garrett Snyder <garrettsnyder0@gmail.com>
2019-02-07 12:12:40 -0800

    Reverse Int added

---
+++ ReverseInt/ReverseInt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInt
+{
+    public class ReverseInt
+    {
+        //my way
+        public static int RevInt(int input)
+        {
+            //string tempResult = "";
+            int temp = input;
+            int result = 0;
+            int count = -1;
+            while(temp != 0)
+            {
+                temp /= 10;
+                count++;
+            }
+    
+
+            while (input != 0)
+            {
+                temp = input % 10;
+                input /= 10;
+                //tempResult += temp;
+                if (count != 0)
+                {
+                    result = result + (temp * (int)Math.Pow(10,count));
+                    count--;
+                }
+                else
+                {
+                    result += temp;
+                }
+            }
+
+
+            return result;
+            //return Int32.Parse(tempResult);
+        }
+
+
+        //Dan's much easier way
+        public static long Mirror(int value)
+        {
+            long sum = 0;
+            for(int i = value; i!= 0; i = i/10)
+            {
+                int remainder = i % 10;
+                sum = sum * 10 + remainder;
+            }
+
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mirror(1073));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReverseIntTests/ReverseIntTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseInt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInt.Tests
+{
+    [TestClass()]
+    public class ReverseIntTests
+    {
+        [TestMethod()]
+        public void RevIntTest()
+        {
+            Assert.AreEqual(3701, ReverseInt.RevInt(1073));
+        }
+
+        [TestMethod()]
+        public void RevIntTest2()
+        {
+            Assert.AreEqual(56746, ReverseInt.RevInt(64765));
+        }
+
+        [TestMethod()]
+        public void RevIntTest3()
+        {
+            Assert.AreEqual(1188344, ReverseInt.RevInt(4438811));
+        }
+
+        [TestMethod()]
+        public void RevIntTest4()
+        {
+            Assert.AreEqual(21, ReverseInt.RevInt(12));
+        }
+    }
+}
